0e86e8aa5675af36c46d6fe6b2285b3963ffec50
lxlung <gavlung@gmail.com>
2024-09-25 18:09:08 -0700

    A:git init src & makefile

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+from constants import *
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+if __name__ == "__main__":
+    main()
